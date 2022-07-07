f25cbf5a71b06aaaf6c475c36842961849ac10e1
edong <edong1111@hotmail.com>
2022-07-07 17:00:32 +0800

    claim in User.Claims

--- IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/WebClient/Program.cs
+++ IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/WebClient/Program.cs
@@ -1,4 +1,4 @@
-using System.IdentityModel.Tokens.Jwt;
+﻿using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,40 +7,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
-
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = "Cookies";
-        options.DefaultChallengeScheme = "oidc";
-    })
-    .AddCookie("Cookies")
-    .AddOpenIdConnect("oidc", options =>
-    {
-        options.Authority = "https://localhost:5001";
+		{
+			options.DefaultScheme = "Cookies";
+			options.DefaultChallengeScheme = "oidc";
+		})
+		.AddCookie("Cookies")
+		.AddOpenIdConnect("oidc", options =>
+		{
+			options.Authority = "https://localhost:5001";
 
-        options.ClientId = "web";
-        options.ClientSecret = "secret";
-        options.ResponseType = "code";
+			options.ClientId = "web";
+			options.ClientSecret = "secret";
+			options.ResponseType = "code";
 
-        options.SaveTokens = true;
+			options.SaveTokens = true;
 
-        options.Scope.Clear();
-        options.Scope.Add("openid");
-        options.Scope.Add("profile");
-        options.Scope.Add("offline_access");
-        options.Scope.Add("api1");
+			options.Scope.Clear();
+			options.Scope.Add("openid");
+			options.Scope.Add("profile");
+			options.Scope.Add("offline_access");
+			options.Scope.Add("api1");
 
-        options.GetClaimsFromUserInfoEndpoint = true;
-    });
+			options.GetClaimsFromUserInfoEndpoint = true;
+		});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
