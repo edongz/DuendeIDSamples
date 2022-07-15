19c7f619e54b40435b449ef85625155024fe0d3e
edong <edong1111@hotmail.com>
2022-07-15 16:19:29 +0800

    测试通过

--- IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/IdentityServerAspNetIdentity/Models/ApplicationUser.cs
+++ IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/IdentityServerAspNetIdentity/Models/ApplicationUser.cs
@@ -1,13 +1,17 @@
 ﻿// Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
 namespace IdentityServerHost.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
 	[PersonalData]
+	[Display(Name = "真实姓名")]
 	public string? Name { get; set; }
 	[PersonalData]
+	[Display(Name = "生日")]
 	public DateTime DOB { get; set; }
 }
--- IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/IdentityServerAspNetIdentity/Pages/UserList.cshtml.cs
+++ IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/IdentityServerAspNetIdentity/Pages/UserList.cshtml.cs
@@ -6,20 +6,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityServerAspNetIdentity.Pages
 {
-	public class UserListModel : PageModel
-	{
-		public readonly ApplicationDbContext _db_context;
-		public UserListModel(ApplicationDbContext db_context)
-		{
-			_db_context = db_context;
+  public class UserListModel : PageModel
+  {
+    public readonly ApplicationDbContext _db_context;
+    public UserListModel(ApplicationDbContext db_context)
+    {
+      _db_context = db_context;
+    }
 
-		}
+    public IList<ApplicationUser>? LstUsers { get; set; }
 
-		public IList<ApplicationUser>? LstUsers { get; set; }
-
-		public async Task OnGetAsync()
-		{
-			await _db_context.Users.ToListAsync();
-		}
-	}
+    public async Task OnGetAsync()
+    {
+      LstUsers = await _db_context.Users.ToListAsync();
+    }
+  }
 }
