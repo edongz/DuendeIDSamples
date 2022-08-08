cd3080be6b6436a895bd65357495e5cc1bfd82d9
edong <edong1111@hotmail.com>
2022-08-08 11:29:40 +0800

    ProfileService 自定义claim

--- IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/IdentityServerAspNetIdentity/CustomProfileService.cs
+++ IdentityServer/v6/Quickstarts/5_AspNetIdentity/src/IdentityServerAspNetIdentity/CustomProfileService.cs
@@ -6,22 +6,22 @@ using System.Security.Claims;
 
 namespace IdentityServerAspNetIdentity
 {
-    public class CustomProfileService : ProfileService<ApplicationUser>
+  public class CustomProfileService : ProfileService<ApplicationUser>
+  {
+    public CustomProfileService(UserManager<ApplicationUser> userManager, IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory) : base(userManager, claimsFactory)
     {
-        public CustomProfileService(UserManager<ApplicationUser> userManager, IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory) : base(userManager, claimsFactory)
-        {
-        }
-
-        protected override async Task GetProfileDataAsync(ProfileDataRequestContext context, ApplicationUser user)
-        {
-            var principal = await GetUserClaimsAsync(user);
-            var id = (ClaimsIdentity)principal.Identity;
-            if (!string.IsNullOrEmpty(user.FavoriteColor))
-            {
-                id.AddClaim(new Claim("favorite_color", user.FavoriteColor));
-            }
-
-            context.AddRequestedClaims(principal.Claims);
-        }
     }
+
+    protected override async Task GetProfileDataAsync(ProfileDataRequestContext context, ApplicationUser user)
+    {
+      var principal = await GetUserClaimsAsync(user);
+      var id = (ClaimsIdentity)principal.Identity;
+      if (!string.IsNullOrEmpty(user.FavoriteColor))
+      {
+        id.AddClaim(new Claim("favorite_color", user.FavoriteColor));
+      }
+
+      context.AddRequestedClaims(principal.Claims);
+    }
+  }
 }
